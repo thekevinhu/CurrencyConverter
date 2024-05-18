d4a77be08f04de5339ccc650363f508c330d67ac
Kevin Hu <22kevinhu@gmail.com>
2024-05-18 13:57:53 -0500

    updated output

--- src/main/java/org/mycompany/currencyconverter/CurrencyConverterServlet.java
+++ src/main/java/org/mycompany/currencyconverter/CurrencyConverterServlet.java
@@ -5,6 +5,7 @@ import java.io.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import java.text.DecimalFormat;
 
 @WebServlet(name = "currencyConverterServlet", value = "/currency-converter-servlet")
 public class CurrencyConverterServlet extends HttpServlet {
@@ -19,12 +20,15 @@ public class CurrencyConverterServlet extends HttpServlet {
 		try {
 			double originalCurrency = Double.parseDouble(originalCurrencyStr);
 			double convertedCurrency = CurrencyConverter.convert(originalCurrency, fromCurrency, toCurrency);
+			DecimalFormat df = new DecimalFormat("#.##");
+			convertedCurrency = Double.valueOf(df.format(convertedCurrency));
 
 			// generate the response HTML
 			response.setContentType("text/html");
 			PrintWriter out = response.getWriter();
 			String htmlResponse = "<html>";
-			htmlResponse += "<body><h2>Conversion Result</h2>";
+			htmlResponse="<style> h2{font-size: 40px; text-align:center;} p{text-align:center;}</style>";
+			htmlResponse += "<body><h2>Conversion Result:</h2>";
 			htmlResponse += "<p>Original Currency: " + originalCurrency + " " + fromCurrency + " = " + convertedCurrency +
 					" " + toCurrency + "</p>";
 			htmlResponse += "</body></html>";
